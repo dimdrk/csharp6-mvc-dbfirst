abea821582ed3a4c363a39263ea39486979983e4
DimDrk <dimdrk@gmail.com>
2024-11-29 22:17:30 +0200

    ITeacherRepository created

---
+++ Repositories/ITeacherRepository.cs
@@ -0,0 +1,13 @@
+﻿using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public interface ITeacherRepository
+    {
+        Task<List<Course>> GetTeacherCoursesAsync(int id);
+        Task<Teacher> GetByPhoneNumberAsync(string phoneNumber);
+        Task<List<User>> GetAllUsersTeachersAsync();
+        Task<List<User>> GetAllUsersTeachersPaginatedAsync(int pageNumber, int pageSize);
+        Task<User?> GetUserTeacherByUsernameAsync(string username);
+    }
+}
