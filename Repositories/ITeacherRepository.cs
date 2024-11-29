625eaac3205156d121b9b52e45a8732afa08c168
DimDrk <dimdrk@gmail.com>
2024-11-29 23:11:33 +0200

    TeacherRepository created

--- Repositories/ITeacherRepository.cs
+++ Repositories/ITeacherRepository.cs
@@ -6,7 +6,7 @@ namespace SchoolApp.Repositories
     public interface ITeacherRepository
     {
         Task<List<Course>> GetTeacherCoursesAsync(int id);
-        Task<Teacher> GetByPhoneNumberAsync(string phoneNumber);
+        Task<Teacher?> GetByPhoneNumberAsync(string phoneNumber);
         Task<List<User>> GetAllUsersTeachersAsync();
         Task<List<User>> GetAllUsersTeachersPaginatedAsync(int pageNumber, int pageSize);
         Task<User?> GetUserTeacherByUsernameAsync(string username);
