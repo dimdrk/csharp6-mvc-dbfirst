a9d73bb62fdcd143996d50f9225c514e3c88cd20
DimDrk <dimdrk@gmail.com>
2024-11-30 14:46:53 +0200

    IUnitOfWork created

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿
+using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Mvc6DbContext _context;
+
+        public UnitOfWork(Mvc6DbContext context)
+        {
+            _context = context;
+        }
+
+        // public UserRepository UserRepository => new(_context);  ToDo
+
+        public TeacherRepository TeacherRepository => new(_context);
+
+        public StudentRepository StudentRepository => new(_context);
+
+        public CourseRepository CourseRepository => new(_context);
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
