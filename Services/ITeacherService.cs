c703535561fbb19939b0f69f3aafb124735f3780
DimDrk <dimdrk@gmail.com>
2024-12-02 10:39:41 +0200

    Teacher Service created

---
+++ Services/ITeacherService.cs
@@ -0,0 +1,14 @@
+﻿using SchoolApp.Data;
+using SchoolApp.DTO;
+
+namespace SchoolApp.Services
+{
+    public interface ITeacherService
+    {
+        Task SignUpUserAsync(TeacherSignupDTO request);
+        Task<List<UserLoginDTO>> GetAllUsersTeachersAsync();
+        Task<List<UserLoginDTO>> GetAllUsersTeachersAsync(int pageNumber, int pageSize);
+        Task<int> GetTeacherCountAsync();
+        Task<User?> GetTeacherByUsernameAsync(string username);
+    }
+}
---
+++ Services/TeacherService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using SchoolApp.Data;
+using SchoolApp.DTO;
+using SchoolApp.Repositories;
+
+namespace SchoolApp.Services
+{
+    public class TeacherService : ITeacherService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<TeacherService> _logger;
+
+        public TeacherService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<TeacherService> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public Task<List<UserLoginDTO>> GetAllUsersTeachersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserLoginDTO>> GetAllUsersTeachersAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetTeacherByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetTeacherCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignUpUserAsync(TeacherSignupDTO request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
